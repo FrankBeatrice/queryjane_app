8d606079faa9b578ab0f234638090726107ed772
Andrés Quiroga <andres.quiroga@roadhousestudio.co>
2017-12-04 22:32:11 -0500

    Removed databases

--- app/settings.py
+++ app/settings.py
@@ -65,15 +65,15 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
 
 
 VALIDATOR = 'django.contrib.auth.password_validation.{0}'
