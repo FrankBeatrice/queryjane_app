5ed5e3a67e25f8f29ee1a8243aa2b90f02944c9e
Andrés Quiroga <andres.quiroga@roadhousestudio.co>
2018-01-05 23:06:57 -0500

    Added user categories update

--- app/static/js/account/profile_update.js
+++ app/static/js/account/profile_update.js
@@ -4,4 +4,28 @@ $(function () {
     $('.QjaneShareGPSfigure, .QjaneShareGPStext').on('click', function() {
         getLocation();
     });
+
+    // Update professional profile category
+    var pp_category_url = $('.qjane-industry-categories-list').data('update-professional-profile-category-url');
+
+    $(".qjane-industry-categories-list").on('click', '.btn', function () {
+        var category_id = $(this).data('category-id');
+        var new_status;
+
+        if ( $( this ).hasClass( "btn-ghost-purple" ) ) {
+            // Add category
+            $( this ).removeClass( "btn-ghost-purple" );
+            $( this ).addClass( "btn-primary" );
+            new_status = true
+        } else {
+            // Remove category
+            $( this ).addClass( "btn-ghost-purple" );
+            $( this ).removeClass( "btn-primary" );
+            new_status = false
+        }
+
+        $.post(pp_category_url, {'category_id': category_id, 'new_status': new_status}, function (response) {
+            console.log("response");
+        });
+    });
 })
