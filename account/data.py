f093340fca3feaeb90f88264c18576a03ff63512
Andrés Quiroga <quid.andres@gmail.com>
2018-03-15 15:16:48 -0500

    Integrated i18n to python files on account application

--- account/data.py
+++ account/data.py
@@ -1,7 +1,9 @@
+from django.utils.translation import ugettext as _
+
 NEW_ENTREPRENEUR_ADMIN = 10
 NEW_JOB_OFFER = 20
 
 NOTIFICATION_TYPE_CHOICES = (
-    (NEW_ENTREPRENEUR_ADMIN, 'Invitation to administer company.'),
-    (NEW_JOB_OFFER, 'New job offer that may interest you.'),
+    (NEW_ENTREPRENEUR_ADMIN, _('Invitation to administer company.')),
+    (NEW_JOB_OFFER, _('New job offer that may interest you.')),
 )
