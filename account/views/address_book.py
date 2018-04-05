35c3bb5819746d08056d9a1f4e86eaabca0b81d0
Andrés Quiroga <quid.andres@gmail.com>
2018-04-04 19:58:23 -0500

    Created address book 'contacts' section, to view the list of added contacts

--- account/views/address_book.py
+++ account/views/address_book.py
@@ -16,6 +16,9 @@ class AddressBookView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['contacts'] = UserContact.objects.filter(
+            owner=self.request.user.professionalprofile,
+        )
         return context
 
 
