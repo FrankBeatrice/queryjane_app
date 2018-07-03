07456ea3f350a326ff229ebe3351572f58b443bb
Andrés Quiroga <quid.andres@gmail.com>
2018-07-03 18:06:49 -0500

    Added footer styles

--- app/static/landing_page/js/main.js
+++ app/static/landing_page/js/main.js
@@ -1,34 +1,3 @@
-// Add your javascript here
-// Don't forget to add it into respective layouts where this js file is needed
 $(document).ready(function() {
-  AOS.init( {
-    // uncomment below for on-scroll animations to played only once
-    // once: true  
-  }); // initialize animate on scroll library
+  AOS.init();
 });
-
-
-// Light Box
-$(document).on("click", '[data-toggle="lightbox"]', function(event) {
-  event.preventDefault();
-  $(this).ekkoLightbox();
-});
-
-// Scroll Top
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-
-  $("#scrolltop").click(function () {
-    $("html,body").animate({ scrollTop: $("#top").offset().top }, "1000");
-    return false
-  })
-})
-
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 500) {
-    $('#scrolltop:hidden').stop(true, true).fadeIn();
-  } else {
-    $('#scrolltop').stop(true, true).fadeOut();
-  }
-});
-
