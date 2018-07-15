7da78684372197652f2600624dc285a60a43ef5a
Andrés Quiroga <quid.andres@gmail.com>
2018-07-15 12:52:00 -0500

    Added code docs for corporative viewa

--- corporative/urls.py
+++ corporative/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url
 
 from .views import ActivateJobOfferView
-from .views import ActivateVentureView
+from .views import ActivateCompanyView
 from .views import AdminDashboardView
 from .views import HideJobOfferView
-from .views import HideVentureView
+from .views import HideCompanyView
 from .views import LegalItemView
 from .views import LegalItemFormView
 from .views import TwitterShareJobView
-from .views import TwitterShareVentureView
+from .views import TwitterShareCompanyView
 from .views import LegalItemsAgreeView
 
 urlpatterns = [
@@ -20,8 +20,8 @@ urlpatterns = [
 
     url(
         r'^ax-twitter-share/(?P<slug>[-\w]+)/venture/$',
-        TwitterShareVentureView.as_view(),
-        name='ax_twitter_share_venture',
+        TwitterShareCompanyView.as_view(),
+        name='ax_twitter_share_company',
     ),
 
     url(
@@ -32,14 +32,14 @@ urlpatterns = [
 
     url(
         r'^ax-hide/(?P<slug>[-\w]+)/venture/$',
-        HideVentureView.as_view(),
-        name='ax_hide_venture',
+        HideCompanyView.as_view(),
+        name='ax_hide_company',
     ),
 
     url(
         r'^ax-activate/(?P<slug>[-\w]+)/venture/$',
-        ActivateVentureView.as_view(),
-        name='ax_activate_venture',
+        ActivateCompanyView.as_view(),
+        name='ax_activate_company',
     ),
 
     url(
@@ -52,12 +52,6 @@ urlpatterns = [
         r'^ax-activate/(?P<slug>[-\w]+)/job_offer/$',
         ActivateJobOfferView.as_view(),
         name='ax_activate_job_offer',
-    ),
-
-    url(
-        r'^ax-twitter-share/(?P<slug>[-\w]+)/venture/$',
-        TwitterShareVentureView.as_view(),
-        name='ax_twitter_share_venture',
     ),
 
     url(
