be14242e04e26eafdfdbb58220088957ad18ddfe
cunigarro <cunigarrod@gmail.com>
2018-08-25 17:20:26 -0500

    Removed old static files.

--- app/static/src/js/landing_page.js
+++ app/static/src/js/landing_page.js
@@ -1,4 +1,12 @@
+// Dependencies
 import './new_layout.js';
+
+// CSS
+import './../sass/landing_page.scss';
+
+// Images
+import './../img/landing_page/register-banner.png';
+import './../img/landing_page/profiles-banner.png';
 
 var AOS = require('aos');
 
